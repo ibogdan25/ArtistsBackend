7475438e49cc967f26d2f612af527ca3d2afb77b
Hasmasan Dragos <hasmasandragos13@gmail.com>
2019-11-25 21:02:24 +0200

    Created Organizer entity

---
+++ src/main/java/model/Organizer.java
@@ -0,0 +1,32 @@
+package model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "organizers")
+@Getter @Setter
+public class Organizer {
+    @Id
+    @GeneratedValue
+    @Column(name = "organizer_id")
+    private Integer artistId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "url_photo_cover")
+    private String urlPhotoCover;
+
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+}
