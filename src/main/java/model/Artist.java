eb55f9f7dcbb9f7091626bb50e6ac1d83cca6706
Ancuta <ancutahij@gmail.com>
2020-01-11 12:32:45 +0200

    Added Address entity

--- src/main/java/model/Artist.java
+++ src/main/java/model/Artist.java
@@ -1,12 +1,12 @@
 package model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import lombok.Getter;
-import lombok.Setter;
+        import com.fasterxml.jackson.annotation.JsonBackReference;
+        import com.fasterxml.jackson.annotation.JsonIgnore;
+        import com.fasterxml.jackson.annotation.JsonManagedReference;
+        import lombok.Getter;
+        import lombok.Setter;
 
-import javax.persistence.*;
+        import javax.persistence.*;
 
 @Entity
 @Table(name = "artists")
@@ -22,6 +22,20 @@ public class Artist {
 
     @Column(name = "description")
     private String description;
+
+    @Column(name = "avatar_url")
+    private String avatarUrl;
+
+    @Column(name = "cover_url")
+    private String coverUrl;
+
+    @Column(name = "stars")
+    private Integer stars;
+
+//    @Column(name = "contact_info")
+//    private ContactInfo contactInfo;
+//
+
 
     @ManyToOne
     @JoinColumn(name="user_id", nullable = false)
