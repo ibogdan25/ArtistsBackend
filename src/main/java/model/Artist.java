04c5f4347aad62ac162480c33480628499b503d7
Ancuta <ancutahij@gmail.com>
2020-01-11 13:46:06 +0200

    update artist

--- src/main/java/model/Artist.java
+++ src/main/java/model/Artist.java
@@ -5,7 +5,6 @@ package model;
         import com.fasterxml.jackson.annotation.JsonManagedReference;
         import lombok.Getter;
         import lombok.Setter;
-
         import javax.persistence.*;
 
 @Entity
