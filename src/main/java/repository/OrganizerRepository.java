4c70f456cc3c4ac3742e8db40b573131840ed08a
ILIE IONUT BOGDAN <ilieibogdan@gmail.com>
2020-01-11 18:46:25 +0200

    Deleted Organizer

--- src/main/java/repository/OrganizerRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import model.Organizer;
-import model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrganizerRepository extends JpaRepository<Organizer, Long> {}
